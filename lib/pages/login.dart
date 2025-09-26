84a2e2ebdb9ed89dc67e4408eb746ec208728f30
prim1112 <65011212063@msu.ac.th>
2025-09-26 13:43:49 +0700

    up

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -250,7 +250,7 @@ class _LoginPageState extends State<LoginPage> {
 
           Navigator.pushReplacement(
             context,
-            MaterialPageRoute(builder: (context) => const SenderPage()),
+            MaterialPageRoute(builder: (context) => const SenderPage(name: '')),
           );
         } else if (data['role'] == "riders") {
           Navigator.pushReplacement(
